bf3a6f59662bfd9a4b8f6dd59e0026885025a340
Simon Thörnqvist <simon.thornqvist@erlang-solutions.com>
2021-10-05 11:22:28 +0200

    prepare for pre-release

--- mix.exs
+++ mix.exs
@@ -1,13 +1,18 @@
 defmodule PGQuery.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/hydradb/pg_query"
   def project do
     [
       app: :pg_query,
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: "",
+      deps: deps(),
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -18,12 +23,32 @@ defmodule PGQuery.MixProject do
     ]
   end
 
+  defp package do
+    %{
+      files: ["lib", "native", "mix.exs", "README.md"],
+      maintainers: ["Simon Thörnqvist"],
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => @source_url
+      }
+    }
+  end
+
+  defp docs do
+    [
+      main: "pg_query",
+      source_url: @source_url,
+      source_ref: "v#{@version}"
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:rustler, "~> 0.22.0"},
       {:jason, "~> 1.2"},
       {:protobuf, github: "elixir-protobuf/protobuf"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:benchee, "~> 1.0", only: :dev}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
